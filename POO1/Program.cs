c4f69729e181ee95d1c8e357417f4e6cdbe37cf6
camfeitosa <camilafeitosacoutinho@gmail.com>
2025-04-02 22:59:17 -0300

    Aula Membros estaticos

--- POO1/Program.cs
+++ POO1/Program.cs
@@ -6,7 +6,22 @@ namespace POO1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("Entre a altura e a largura de um retangulo: ");
+
+            retangulo.altura = int.Parse(Console.ReadLine());
+            Console.WriteLine(retangulo.altura);
+            retangulo.largura = int.Parse(Console.ReadLine());
+            Console.WriteLine(retangulo.largura);
+
+            double area = retangulo.Area();
+            double perimetro = retangulo.Perimetro();
+
+            Console.WriteLine($"Area: {area} ");
+            Console.WriteLine($"Perimetro: {perimetro}");
+
+
         }
     }
 }
--- POO1/Retangulo.cs
+++ POO1/Retangulo.cs
@@ -4,13 +4,21 @@ namespace POO1
 {
     internal class Retangulo
     {
-        public double largura, altura;
+        public int largura, altura;
 
         public double Area()
         {
             double area = largura * altura;
             return area;
         }
+
+        public double Perimetro()
+        {
+            double perimetro = 2 * (largura + altura);
+            return perimetro;
+        }
+
+
 
     }
 }
