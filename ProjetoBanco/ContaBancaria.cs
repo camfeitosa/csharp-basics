3cc3616156d33e114fafef995fc30f3ada0faaad
camfeitosa <camilafeitosacoutinho@gmail.com>
2025-04-19 13:02:10 -0300

    Início projeto Banco

---
+++ ProjetoBanco/ContaBancaria.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ProjetoBanco
+{
+    internal class ContaBancaria
+    {
+        private double _depositoInicial;
+        public int NumeroConta { get ; private set; }
+        public string Nome { get; set; }
+        public double Saldo { get; private set; }
+
+ 
+        public ContaBancaria(int numeroConta, string nome )
+        {
+            _depositoInicial = 0.00;
+            NumeroConta = numeroConta;
+            Nome = nome;
+        }
+
+        public ContaBancaria(int numeroConta, string nome, double depositoInicial) : this(numeroConta, nome)
+        {
+            _depositoInicial = depositoInicial;
+        }
+
+        public void Depositar()
+        {
+
+        }
+
+        public void Sacar()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + NumeroConta + ", Titular: " + Nome + ", Saldo: " + Saldo;
+        }
+
+
+
+    }
+}
---
+++ ProjetoBanco/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoBanco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
